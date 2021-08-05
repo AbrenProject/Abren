5a9406026c3a1bbec90dccdea334574ed18fbc52
Amen Sime <amensime@gmail.com>
2021-08-05 08:31:22 +0300

    commented Mapbox

--- app/src/main/java/com/example/abren/RequestList.kt
+++ app/src/main/java/com/example/abren/RequestList.kt
@@ -1,67 +1,35 @@
 package com.example.abren
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+//import com.mapbox.mapboxsdk.Mapbox
+//import com.mapbox.mapboxsdk.maps.MapView
+//import com.mapbox.mapboxsdk.maps.Style
 
 
-class RequestList (){
+class RequestList: AppCompatActivity (){
 
-    private var mapView: MapView? = null
+//    private var mapView: MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+//        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
 
         setContentView(R.layout.activity_main)
 
-        mapView = findViewById(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
+//        mapView = findViewById(R.id.mapView)
+//        mapView?.onCreate(savedInstanceState)
+//        mapView?.getMapAsync { mapboxMap ->
 
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+//            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
 
                 // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
 
-            }
+//            }
 
         }
     }
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
 
-    override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView?.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView?.onDestroy()
-    }
-
-
-
-}
+//}
