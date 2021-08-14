2f858387a66ccdaf980406de64105fe01914f07e
Amen Sime <amensime@gmail.com>
2021-08-14 18:10:16 +0300

    updated profile

--- app/src/main/java/com/example/abren/UserProfile.kt
+++ app/src/main/java/com/example/abren/UserProfile.kt
@@ -1,4 +1,16 @@
 package com.example.abren
 
-class UserProfile {
-}
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class UserProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.profile_page, container, false)
+    }}
